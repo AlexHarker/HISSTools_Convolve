c257278b8ccbc47cda7324e143bf2e9a720d5ab0
Alex Harker <ajharker@gmail.com>
2023-02-12 00:01:08 +0000

    Update config.h

--- config.h
+++ config.h
@@ -1,50 +1,46 @@
-#define PLUG_MFR "AHarker"
-#define PLUG_NAME "AHConvN"
+#define PLUG_NAME "HISSTools Convolve"
+#define PLUG_MFR "Alex Harker"
+#define PLUG_VERSION_HEX 0x00010000
+#define PLUG_VERSION_STR "1.0.0"
+#define PLUG_UNIQUE_ID 'HTCn'
+#define PLUG_MFR_ID 'AHAR'
+#define PLUG_URL_STR "www.alexanderjharker.co.uk"
+#define PLUG_EMAIL_STR "A.Harker@hud.ac.uk"
+#define PLUG_COPYRIGHT_STR  "Copyright 2018-2022 Alex Harker"
+#define PLUG_CLASS_NAME HISSToolsConvolve
 
-#define PLUG_CLASS_NAME AHConvN
-
+#define BUNDLE_NAME "HISSToolsConvolve"
+#define BUNDLE_MFR "AlexHarker"
 #define BUNDLE_DOMAIN "com"
-#define BUNDLE_MFR "AHarker"
-#define BUNDLE_NAME "AHConvN"
+
+#define AUV2_ENTRY HISSToolsConvolve_Entry
+#define AUV2_ENTRY_STR "HISSToolsConvolve_Entry"
+#define AUV2_FACTORY HISSToolsConvolve_Factory
+#define AUV2_VIEW_CLASS HISSToolsConvolve_View
+#define AUV2_VIEW_CLASS_STR "HISSToolsConvolve_View"
+
+#define APP_SIGNAL_VECTOR_SIZE 64
 
 #define PLUG_WIDTH   340
 #define PLUG_HEIGHT  480
-
-#define PLUG_ENTRY AHConvN_Entry
-#define PLUG_VIEW_ENTRY AHConvN_ViewEntry
-
-#define PLUG_ENTRY_STR "AHConvN_Entry"
-#define PLUG_VIEW_ENTRY_STR "AHConvN_ViewEntry"
-
-#define VIEW_CLASS AHConvN_View
-#define VIEW_CLASS_STR "AHConvN_View"
-
-// Format        0xMAJR.MN.BG - in HEX! so version 10.1.5 would be 0x000A0105
-#define PLUG_VERSION_HEX 0x00010000
-#define VST3_VER_STR "1.0.0"
-
-// http://service.steinberg.de/databases/plugin.nsf/plugIn?openForm
-// 4 chars, single quotes. At least one capital letter
-#define PLUG_UNIQUE_ID 'AhNC'
-// make sure this is not the same as BUNDLE_MFR
-#define PLUG_MFR_ID 'AHar'
+#define PLUG_HOST_RESIZE 1
 
 // ProTools stuff
 #define PLUG_MFR_DIGI "AHarker\nAHarker\nAHar\n"
 #define PLUG_NAME_DIGI "AHConvN\nIPEF"
 #define EFFECT_TYPE_DIGI "Effect" // valid options "None" "EQ" "Dynamics" "PitchShift" "Reverb" "Delay" "Modulation" "Harmonic" "NoiseReduction" "Dither" "SoundField" "Effect" instrument determined by PLUG _IS _INST
 
-
 #define MAX_CHANNELS 8
 #define PLUG_CHANNEL_IO "1-1 2-2 3-3 4-4 5-5 6-6 7-7 8-8"
 #define PLUG_LATENCY 0
-#define PLUG_IS_INSTRUMENT 0
-#define PLUG_HAS_UI 1
-
-// if this is 0 RTAS can't get tempo info
-#define PLUG_DOES_MIDI 0
-
+#define PLUG_TYPE 0
+#define PLUG_DOES_MIDI_IN 0
+#define PLUG_DOES_MIDI_OUT 0
+#define PLUG_DOES_MPE 0
 #define PLUG_DOES_STATE_CHUNKS 1
+#define PLUG_FPS 60
+#define PLUG_HAS_UI 1
+#define PLUG_SHARED_RESOURCES 0
 
 // on MSVC, you must define SA_API in the resource editor preprocessor macros as well as the c++ ones
 #ifdef SA_API
